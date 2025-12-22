555a2672e26d13c8b79d92c82562ea6ab19f7aa1
Francois Perrad <francois.perrad@gadz.org>
2025-12-22 18:25:13 +0100

    Add support for Lua 5.5.0
    note: at this time, not supported by luarocks 3

--- hererocks.py
+++ hererocks.py
@@ -913,19 +913,18 @@ class RioLua(Lua):
         "5.2.0", "5.2.1", "5.2.2", "5.2.3", "5.2.4",
         "5.3.0", "5.3.1", "5.3.2", "5.3.3", "5.3.4", "5.3.5", "5.3.6",
         "5.4.0", "5.4.1", "5.4.2", "5.4.3", "5.4.4", "5.4.5", "5.4.6", "5.4.7", "5.4.8",
-        "5.5.0-rc4"
+        "5.5.0"
     ]
     translations = {
-        "5": "5.4.8",
+        "5": "5.5.0",
         "5.1": "5.1.5",
         "5.1.0": "5.1",
         "5.2": "5.2.4",
         "5.3": "5.3.6",
         "5.4": "5.4.8",
-        "5.5": "5.5.0-rc4",
-        "5.5.0": "5.5.0-rc4",
-        "^": "5.4.8",
-        "latest": "5.4.8"
+        "5.5": "5.5.0",
+        "^": "5.5.0",
+        "latest": "5.5.0"
     }
     checksums = {
         "lua-5.1.tar.gz"        : "7f5bb9061eb3b9ba1e406a5aa68001a66cb82bac95748839dc02dd10048472c1",
@@ -955,7 +954,7 @@ class RioLua(Lua):
         "lua-5.4.6.tar.gz"      : "7d5ea1b9cb6aa0b59ca3dde1c6adcb57ef83a1ba8e5432c0ecd06bf439b3ad88",
         "lua-5.4.7.tar.gz"      : "9fbf5e28ef86c69858f6d3d34eccc32e911c1a28b4120ff3e84aaa70cfbf1e30",
         "lua-5.4.8.tar.gz"      : "4f18ddae154e793e46eeab727c59ef1c0c0c2b744e7b94219710d76f530629ae",
-        "lua-5.5.0-rc4.tar.gz"  : "57ccc32bbbd005cab75bcc52444052535af691789dba2b9016d5c50640d68b3d",
+        "lua-5.5.0.tar.gz"      : "57ccc32bbbd005cab75bcc52444052535af691789dba2b9016d5c50640d68b3d",
     }
     all_patches = {
         "When loading a file, Lua may call the reader function again after it returned end of input": """
@@ -3156,8 +3155,8 @@ def main(argv=None):
     parser.add_argument(
         "-l", "--lua", help="Version of standard PUC-Rio Lua to install. "
         "Version can be specified as a version number, e.g. 5.2 or 5.3.1. "
-        "Versions 5.1.0 - 5.4.8 and 5.5.0-rc4 are supported. "
-        "'latest' and '^' are aliases for 5.4.8. "
+        "Versions 5.1.0 - 5.5.0 are supported. "
+        "'latest' and '^' are aliases for 5.5.0. "
         "If the argument contains '@', sources will be downloaded "
         "from a git repo using URI before '@' and using part after '@' as git reference "
         "to checkout, 'master' by default. "
